158a1e49a9b4e8529afd95ee9ba44c809d6859b5
densel.esekon2@student.moringaschool.com <esekondensel@gmail.com>
2023-10-24 20:52:30 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,38 +1,60 @@
-// Write your solution here!
-const cats = ["Milo", "Otis", "Garfield", "Garrfield"];
+//Write your solutions here
+const cats = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendCat(name) {
+ 
   cats.push(name);
 }
 
 function destructivelyPrependCat(name) {
+  
   cats.unshift(name);
 }
 
 function destructivelyRemoveLastCat() {
+  
   cats.pop();
 }
 
 function destructivelyRemoveFirstCat() {
+  
   cats.shift();
 }
 
 function appendCat(name) {
-  const newCats = [...cats, name];
-  return newCats;
+   
+  return [...cats, name];
 }
 
 function prependCat(name) {
-  const newCats = [name, ...cats];
-  return newCats;
+   
+  return [name, ...cats];
 }
 
 function removeLastCat() {
-  const newCats = cats.slice(0, -1);
-  return newCats;
+
+  return cats.slice(0, -1);
 }
 
 function removeFirstCat() {
-  const newCats = cats.slice(1);
-  return newCats;
+  
+  return cats.slice(1);
 }
+
+module.exports = {
+  destructivelyAppendCat,
+  destructivelyPrependCat,
+  destructivelyRemoveLastCat,
+  destructivelyRemoveFirstCat,
+  appendCat,
+  prependCat,
+  removeLastCat,
+  removeFirstCat,
+};
+
+
+
+
+
+
+
