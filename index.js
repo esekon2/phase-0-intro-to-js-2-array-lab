4bef9cb71eac29b52fbf3edc39104cbb91f902a9
densel.esekon2@student.moringaschool.com <esekondensel@gmail.com>
2023-10-24 21:10:37 +0300

    Assignment Updated

--- index.js
+++ index.js
@@ -1,60 +1,59 @@
-//Write your solutions here
+// Write your code here
 const cats = ["Milo", "Otis", "Garfield"];
 
+// Destructively appends a cat to the end of the cats array
 function destructivelyAppendCat(name) {
- 
-  cats.push(name);
+    cats.push(name); // This line is correct and modifies the original cats array.
 }
 
+// Destructively prepends a cat to the beginning of the cats array
 function destructivelyPrependCat(name) {
-  
-  cats.unshift(name);
+    cats.unshift(name); // This line is correct and modifies the original cats array.
 }
 
+// Destructively removes the last cat from the cats array
 function destructivelyRemoveLastCat() {
-  
-  cats.pop();
+    cats.pop(); // This line is correct and modifies the original cats array.
 }
 
+// Destructively removes the first cat from the cats array
 function destructivelyRemoveFirstCat() {
-  
-  cats.shift();
+    cats.shift(); // This line is correct and modifies the original cats array.
 }
 
+// Appends a cat to the cats array and returns a new array, leaving the cats array unchanged
 function appendCat(name) {
-   
-  return [...cats, name];
+    const catsNew = [...cats, name]; // Create a new array without modifying the original cats array.
+    return catsNew;
 }
 
+// Prepends a cat to the cats array and returns a new array, leaving the cats array unchanged
 function prependCat(name) {
-   
-  return [name, ...cats];
+    const catsNew = [name, ...cats]; // Create a new array without modifying the original cats array.
+    return catsNew;
 }
 
+// Removes the last cat in the cats array and returns a new array, leaving the cats array unchanged
 function removeLastCat() {
-
-  return cats.slice(0, -1);
+    const lessCats = cats.slice(0, -1); // Create a new array without modifying the original cats array.
+    return lessCats;
 }
 
+// Removes the first cat from the cats array and returns a new array, leaving the cats array unchanged
 function removeFirstCat() {
-  
-  return cats.slice(1);
+    const lessCats = cats.slice(1); // Create a new array without modifying the original cats array.
+    return lessCats;
 }
 
-module.exports = {
-  destructivelyAppendCat,
-  destructivelyPrependCat,
-  destructivelyRemoveLastCat,
-  destructivelyRemoveFirstCat,
-  appendCat,
-  prependCat,
-  removeLastCat,
-  removeFirstCat,
-};
+// Testing the functions
+const appendedCats = appendCat("Broom");
+console.log(appendedCats);
 
+const prependedCats = prependCat("Arnold");
+console.log(prependedCats);
 
+const catsWithLastRemoved = removeLastCat();
+console.log(catsWithLastRemoved);
 
-
-
-
-
+const catsWithFirstRemoved = removeFirstCat();
+console.log(catsWithFirstRemoved);
